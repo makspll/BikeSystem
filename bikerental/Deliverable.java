ec997ca962ca1486cd0a91a8212e51d959052319
makspll <makspl17@gmail.com>
2019-11-25 21:16:18 +0000

    B please

--- bikerental/Deliverable.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ed.bikerental;
-
-public interface Deliverable {
-    public void onPickup();
-
-    public void onDropoff();
-}
--- bikerental/ValuationPolicy.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.ed.bikerental;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-public interface ValuationPolicy {
-    public BigDecimal calculateValue(Bike bike, LocalDate date);
-}
