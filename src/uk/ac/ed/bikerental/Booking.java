ddc448ea285bfb43e53e8f2d52a5a9ebba549b83
makspll <makspl17@gmail.com>
2019-11-25 17:39:34 +0000

    mlg pro lol haha xd

--- src/uk/ac/ed/bikerental/Booking.java
+++ src/uk/ac/ed/bikerental/Booking.java
@@ -4,29 +4,32 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.LinkedList;
 
-import uk.ac.ed.bikerental.Utils.BookingStatusEnum;
+
+import uk.ac.ed.bikerental.Utils.EBookingStatus;
 
 public class Booking {
 
-	private String orderCode;
+	private int orderCode;
 	private BigDecimal deposit;
 	private BigDecimal price;
-	private BookingStatusEnum status;
-	private LinkedList<String> bikeCodes;
+	private EBookingStatus status;
+	private LinkedList<Integer> bikeCodes;
 	private LocalDate expiryDate;
 	private BikeProvider providerID;
 	
-	public Booking(String pCode, BigDecimal pDeposit, BigDecimal pPrice , LinkedList<String> pBikeCodes , LocalDate pExpiryDate, BikeProvider pProviderID) {
-		orderCode = pCode;
+	private static int UNIQUE_CODE_COUNT = 0;
+	
+	public Booking(BigDecimal pDeposit, BigDecimal pPrice , LinkedList<Integer> pBikeCodes , LocalDate pExpiryDate, BikeProvider pProviderID) {
+		orderCode = ++UNIQUE_CODE_COUNT;									// This might work for now. 
 		deposit = pDeposit;
 		price = pPrice;
-		status = BookingStatusEnum.BOOKED;
+		status = EBookingStatus.BOOKED;
 		bikeCodes = pBikeCodes;
 		expiryDate = pExpiryDate;
 		providerID = pProviderID;
 	}	
 	
-	public void setBookingStatus(BookingStatusEnum newStatus) {
+	public void setBookingStatus(EBookingStatus newStatus) {
 		status = newStatus;
 	}
 	
@@ -34,15 +37,18 @@ public class Booking {
 		// I am not 100% sure what this does, this is just a first skeleton and probably not useful:
 		switch (status) {
 			case BOOKED: 
-				status = orderly ? BookingStatusEnum.PICKEDUP : BookingStatusEnum.BIKESONWAYBACK;
+				status = orderly ? EBookingStatus.PICKED_UP : EBookingStatus.BIKES_ON_WAY_BACK;
 				break;
-			case PICKEDUP:
-				status = orderly ? BookingStatusEnum.RETURNED : BookingStatusEnum.BIKESONWAYBACK;
+			case PICKED_UP:
+				status = orderly ? EBookingStatus.RETURNED : EBookingStatus.BIKES_ON_WAY_BACK;
 				break;
-			case BIKESONWAYBACK:
-				status = BookingStatusEnum.RETURNED;
+			case BIKES_ON_WAY_BACK:
+				status = EBookingStatus.RETURNED;
 				break;
 		}
 	}
+	
+	public int getOrderCode() { return orderCode; }
+	public EBookingStatus getStatus() { return status; }
 	
 }
