81a7be4f2f5462918396bc67c81719651db53f1e
makspll <makspl17@gmail.com>
2019-11-25 21:13:08 +0000

    A pleaase

--- tests/uk/ac/ed/bikerental/SystemTests.java
+++ tests/uk/ac/ed/bikerental/SystemTests.java
@@ -15,14 +15,38 @@ import java.util.TreeSet;
 
 public class SystemTests {
     // You can add attributes here
-
+    DeliveryService ds;
+    BikeRentalSystem bikeSystem;
+    BikeProvider bp1,bp2;
+    Customer customer;
+    DateRange available;
     @BeforeEach
     void setUp() throws Exception {
         // Setup mock delivery service before each tests
         DeliveryServiceFactory.setupMockDeliveryService();
+        ds = DeliveryServiceFactory.getDeliveryService();
+        bp1 = new BikeProvider(0,new Location("EH10 1SY","69 cumming street"), 1,"981293819238","24/7");
+        bp2 = new BikeProvider(1,new Location("EH11 2S3","5R TestBased Street"),1,"12381819312","24/7");
         
-        // Put your test setup here
+        available = new DateRange(new LocalDate(2019,1,1),
+                                new LocalDate(2019,1,10));
+
+        bp1.registerBikeType();
+        bp2.registerBikeType();
+
+        bp1.registerBike();
+        bp2.registerBike();
+
+        customer = new Customer();
+        customer.bikeSystem = bikeSystem;
+        
     }
     
-    // TODO: Write system tests covering the three main use cases
+    @Test
+    void findingQuoteSuccess()
+    {
+        
+        customer.findQuotes();
+    }
+
 }
