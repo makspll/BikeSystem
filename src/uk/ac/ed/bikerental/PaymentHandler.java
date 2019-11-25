7bf7a934e91081e22ccf47d4da9586971d2f8bee
makspll <makspl17@gmail.com>
2019-11-25 19:43:01 +0000

    added payment handler

---
+++ src/uk/ac/ed/bikerental/PaymentHandler.java
@@ -0,0 +1,11 @@
+package uk.ac.ed.bikerental;
+
+public class PaymentHandler
+{
+    public boolean payTo(String details)
+    {
+        //normally this would have i/o
+        //we're assuming everyone pays, correctly when this is called
+        return true;
+    }
+}
